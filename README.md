ef048a375ba6ba23acd3787cd7ce22246149737e
hankshen <sunshen.hx@gmail.com>
2016-04-06 16:16:32 -0400

    updated keyfile

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # RentalEstimates
 
 Apr 3, 2016
+
+##什么也没干
+
+==
+Apr 6. 2016
+
+##可以一起来用google API抓链接啦
+
+先用google帐号在这里注册一个projcet https://console.developers.google.com/apis/, 启用custom search api，把google api的key记下来。
+
+然后在这里 https://cse.google.com/cse/all 创建一个自定义搜索引擎，获取搜索引擎ID
+
+用的代码是`searchForLinks.py`，可以先拷贝一份到本地，把名字改成你们的，比如 `searchForLinks_shx.py`, (这份就可以不用传到github上)。因为每个人要用到的ID都不一样。
+
+然后把你们刚刚申请的对应的Google API key和Custom Search Engine ID填进keyFile里以后就可以跑了（左边API key，右边cse ID，中间用tab隔开）。每天跑一次这个脚本就行。
+
+
--- searchForLinks.py
+++ searchForLinks.py
@@ -5,41 +5,51 @@ from googleapiclient.discovery import build
 def main():
 
   ## change developerKey into your own key
-  service = build("customsearch", "v1",developerKey="AIzaSyCHNsNCSOz6ervN6_zHdJaoDqW-mXac_-o")
-  ## change file name
-  addressFile = open('city_st_clean_shx','r')
-  querys=[]
-  for address in addressFile.readlines()[0:10]:
-    querys.append(address.strip())
-  # 10 addressIndex can be searched per day for each Google account, change range(0,1) to range(0,10)
-  # there should be at most 1000 more links added to the file 'zillow'
-  addressIndex = 0
-  try:
-    for addressIndex in range(0,10):
-      print "search for: "+ querys[addressIndex]
-      for queryIndex in range(0,10):
-        print "search page "+str(queryIndex+1)
+  keys = []
+  keyFile = open('keyfile','r')
+  for line in keyFile.readlines():
+    devKey = line.split('\t')[0]
+    cxKey = line.split('\t')[1]
+    keys.append([devKey,cxKey])
+  for keyIndex in range(len(keys)):
+    service = build("customsearch", "v1",developerKey=keys[keyIndex][0])
+    ## change shx to your name
+    addressFile = open('city_st_clean_shx','r')
+    querys=[]
+    for address in addressFile.readlines()[0:10]:
+      querys.append(address.strip())
+    # 10 addressIndex can be searched per day for each Google account, change range(0,1) to range(0,10)
+    # there should be at most 1000 more links added to the file 'zillow'
+    addressIndex = 0
+    exceed = 1
+    try:
+      for addressIndex in range(0,10):
+        print "search for: "+ querys[addressIndex]
+        for queryIndex in range(0,10):
+          print "search page "+str(queryIndex+1)
 
-        ## change the value of cx to your own search engine ID
-        res = service.cse().list(q=querys[addressIndex],cx='010195169098183863802:9s71hhrm6em',start=1+queryIndex*10).execute()
-        
-        file = open('zillow','a')
-        for item in res['items']:
-          file.write(item['link']+"\n")
-  except:
-    print 'quota use up?'
+          ## change the value of cx to your own search engine ID
+          res = service.cse().list(q=querys[addressIndex],cx=keys[keyIndex][0],start=1+queryIndex*10).execute()
 
-  ## change file name to your own -- jxs -- gjq
-  addressFile = open('city_st_clean_shx','r')
-  addressFile_new = open('city_st_clean_shx_new','w')
+          file = open('zillow','a')
+          for item in res['items']:
+            file.write(item['link']+"\n")
+    except:
+      print 'quota use up?'
+      exceed = 0
 
-  # drop the 10 addresses that have been searched
-  for address in addressFile.readlines()[addressIndex+1:]:
-    addressFile_new.write(address)
-  addressFile.close()
+    ## change file name to your own -- jxs -- gjq
+    addressFile = open('city_st_clean_shx','r')
+    addressFile_new = open('city_st_clean_shx_new','w')
 
-  os.remove('city_st_clean_shx')
-  os.rename('city_st_clean_shx_new','city_st_clean_shx')
+    # drop the 10 addresses that have been searched
+    for address in addressFile.readlines()[addressIndex+exceed:]:
+      addressFile_new.write(address)
+    addressFile.close()
+
+    ## change shx to your name
+    os.remove('city_st_clean_shx')
+    os.rename('city_st_clean_shx_new','city_st_clean_shx')
 
 if __name__ == '__main__':
   main()
